faf59ab91594c24b272e09c25ba4d345521165cc
Srijan Personal <chapssrijan@gmail.com>
2024-12-08 20:16:26 +0545

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-# Download the correct binary for the OS
-curl -LO https://github.com/srijan619/bbpr/releases/download/latest/bbpr-linux-amd64
-chmod +x bbpr-linux-amd64
-sudo mv bbpr-linux-amd64 /usr/local/bin/bbpr
+
+# Detect OS
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+if [ "$OS" = "darwin" ]; then
+  FILE="bbpr-darwin-amd64"
+elif [ "$OS" = "linux" ]; then
+  FILE="bbpr-linux-amd64"
+elif [[ "$OS" == *"mingw"* || "$OS" == *"cygwin"* ]]; then
+  FILE="bbpr-windows-amd64.exe"
+else
+  echo "Unsupported OS"
+  exit 1
+fi
+
+# Download the binary
+echo "Downloading $FILE..."
+curl -L -o bbpr "https://github.com/srijan619E/bbpr-terminal/releases/latest/download/$FILE"
+
+# Make executable and move to a directory in PATH
+chmod +x bbpr
+sudo mv bbpr /usr/local/bin/
+
+echo "Installation complete! Run 'bbpr' to start."/local/bin/bbpr
