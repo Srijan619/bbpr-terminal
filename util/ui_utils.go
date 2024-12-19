e2612fe02985b49964a5a036d423e80b5c82e6e8
Srijan Personal <chapssrijan@gmail.com>
2024-12-19 22:22:42 +0200

    try around active focus border

--- util/ui_utils.go
+++ util/ui_utils.go
@@ -115,20 +115,22 @@ func PopulatePRList(prList *tview.Table, prs []types.PR) {
 }
 
 // Helper method to update borders of views
+
 func UpdateFocusBorders(focusOrder []tview.Primitive, currentFocusIndex int, activeBorderColor tcell.Color) {
-	// for i, view := range focusOrder {
-	// 	// Check if the view has border-related methods
-	// 	// if bordered, ok := view.(interface {
-	// 	// 	SetBorder(bool) *tview.Box
-	// 	// 	SetBorderColor(tcell.Color) *tview.Box
-	// 	// }); ok {
-	// 	// 	if i == currentFocusIndex {
-	// 	// 		// bordered.SetBorder(true).
-	// 	// 		// 	SetBorderColor(activeBorderColor).
-	// 	// 		// 	SetBorderPadding(1, 1, 1, 1)
-	// 	// 	}
-	// 	// }
-	// }
+	for i, view := range focusOrder {
+		// Check if the view has border-related methods (i.e., if it's a tview.Box)
+		if box, ok := view.(*tview.Box); ok {
+			// If this is the currently focused view, update its border
+			if i == currentFocusIndex {
+				box.SetBorder(true)
+				box.SetBorderColor(activeBorderColor)
+				box.SetBorderPadding(1, 1, 1, 1) // Optional: Adjust padding as needed
+			} else {
+				// Reset the border for other views
+				box.SetBorder(false)
+			}
+		}
+	}
 }
 
 func UpdateView(targetView interface{}, content interface{}) {
